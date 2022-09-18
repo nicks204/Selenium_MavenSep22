48d2a296c949af494f5068a218243d167a45e719
Your Name <you@example.com>
2022-09-18 12:11:17 +0530

    first commit

---
+++ src/main/java/com/simplilearn/Seleniium_Maven/App.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Seleniium_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
